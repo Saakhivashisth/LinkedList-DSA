7f92be56eb322a86f5c42e75f778a605fc610e0d
Saakhivashisth <saakhi.vashisth@gmail.com>
2026-04-01 12:43:37 +0530

    Time: 0 ms (100%), Space: 41.8 MB (89.68%) - LeetHub

---
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int fib(int n) {
+        int[] memo=new int[n+1];
+        return helper(n,memo);
+    }
+    private int helper(int n,int[] memo){
+        if (n<=1) return n;
+        if(memo[n]!=0) return memo[n];
+        memo[n]=helper(n-1,memo)+helper(n-2,memo);
+        return memo[n];
+    }
+}
